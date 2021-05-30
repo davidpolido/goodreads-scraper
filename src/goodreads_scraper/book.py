eb3736e37cd23ec4c667aa26abb2ae8958ac26d1
David Polido <dav.polido@gmail.com>
2021-05-30 17:44:51 +0100

    Add retry, language & event_id collection

--- src/goodreads_scraper/book.py
+++ src/goodreads_scraper/book.py
@@ -1,6 +1,8 @@
+import logging
 import re
 
 from bs4 import BeautifulSoup
+from retry import retry
 
 
 def parse_timeline_event(event_raw):
@@ -35,27 +37,43 @@ def parse_timeline_event(event_raw):
         except IndexError:
             edition = None
 
-    return {"date": date, "status": status, "edition": edition, "shelf": shelf}
+    return {
+        "event_date": date,
+        "event_status": status,
+        "event_edition": edition,
+        "event_shelf": shelf,
+    }
 
 
+@retry()
 def parse_book(session, book_id):
+    logging.basicConfig()
     response = session.get(f"https://www.goodreads.com/book/show/{book_id}")
     html = response.text
 
     soup = BeautifulSoup(html, "html.parser")
     title = soup.find("h1", id="bookTitle").get_text().replace("\n", "").strip()
     edition = soup.find("span", attrs={"itemprop": "bookFormat"}).get_text().strip()
+    try:
+        language = soup.find("div", attrs={"itemprop": "inLanguage"}).get_text().strip()
+    except AttributeError:
+        language = None
     timeline_html = soup.find_all("div", class_="readingTimeline__text")
 
     book = []
     for div in timeline_html[::-1]:
         if div.get_text():
             event = parse_timeline_event(div.get_text().strip())
-            event["title"] = title
 
+            a_tag = div.find(href=re.compile("/book/show/.*"))
+            event["event_id"] = (
+                re.search(r"([0-9]+)", a_tag["href"]).group(1) if a_tag else None
+            )
+
+            event["title"] = title
             event["book_id"] = book_id
-            if not event["edition"]:
-                event["edition"] = edition
+            event["edition"] = edition
+            event["language"] = language
 
             book.append(event)
     print(f"Parsed '{title}' (id: {book_id})")
